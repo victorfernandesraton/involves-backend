19a35dd6c4192cf877f88ce29d7b60dc7fcec4f4
victorfernandesraton <vfbraton@gmail.com>
2022-02-06 14:13:54 -0300

    test for a upsert sellpoint

---
+++ test/unit/upsertSellPoint.spec.ts
@@ -0,0 +1,10 @@
+describe("upsertCellpoint", () => {
+  test.todo("should be a upsert a sellpoint");
+  test.todo("should be a upsert a sellpoint using specific service");
+  describe("error", () => {
+    test.todo("should be a error when not find a cellpoint");
+    test.todo(
+      "should be a error when not find a cellpoint in specific service"
+    );
+  });
+});
