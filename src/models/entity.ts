43b16925c3451c1b88e5ff454a4221cb3aa9f173
victorfernandesraton <vfbraton@gmail.com>
2022-02-06 10:38:51 -0300

    implement external id

--- src/models/entity.ts
+++ src/models/entity.ts
@@ -1,15 +1,18 @@
 import { v4 } from "uuid";
 
 export interface IEnttityParams {
+  id_external: string;
   id?: string;
   dt_register?: Date;
   dt_updated?: Date;
 }
 export default class Entity {
   readonly id: string = v4();
+  readonly id_external: string;
   dt_register: Date = new Date();
   dt_updated: Date = new Date();
   constructor(params: IEnttityParams) {
+    this.id_external = params.id_external;
     if (params.id) {
       this.id = params.id;
     }
--- src/models/product.ts
+++ src/models/product.ts
@@ -3,19 +3,16 @@ import Entity, { IEnttityParams } from "./entity";
 export interface IProductParams extends IEnttityParams {
   st_product: string;
   fk_category: string;
-  id_external: string;
   bo_active?: boolean;
 }
 export default class Product extends Entity {
   st_product: string;
   fk_category: string;
-  id_external: string;
   bo_active: boolean = true;
   constructor(params: IProductParams) {
     super(params);
     this.st_product = params.st_product;
     this.fk_category = params.fk_category;
-    this.id_external = params.id_external;
     if (params?.bo_active === true || params.bo_active === false) {
       this.bo_active = params.bo_active;
     }
