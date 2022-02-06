76d27a8317eaf0104f832f9489a3fdac2bb1f0e5
victorfernandesraton <vfbraton@gmail.com>
2022-02-06 10:54:03 -0300

    upsertProduct usecase

---
+++ test/unit/upsertProduct.spec.ts
@@ -0,0 +1,7 @@
+describe("upsertProduct", () => {
+  test.todo("should be a any data", async () => {});
+  test.todo("should be product from specific service", async () => {});
+  describe("errors", () => {
+    test.todo("should be not find product ", async () => {});
+  });
+});
