e0b281590da74553a3aa7b2c50e41fe4e91c636e
victorfernandesraton <vfbraton@gmail.com>
2022-02-06 18:50:48 -0300

    implement a simple insert sell

---
+++ test/unit/insertSell.spec.ts
@@ -0,0 +1,7 @@
+describe("InsertSell", () => {
+  test.todo("should be insert a simple sell");
+  test.todo("should be insert multiple sells");
+  describe("error", () => {
+    test.todo("should error because not valid sellpoint");
+  });
+});
